ad1f3db0543b40b9b7eaca4a24f71e6099745345
jumann157 <jumanmardin2004@gmail.com>
2025-08-11 11:59:24 +0100

    Base 'Library Page' layout of the website

---
+++ javascript/movieForm.js
@@ -0,0 +1,38 @@
+const blurOverlay = document.querySelector(".blur-background");
+const movieFormOne = document.querySelector(".movie-form.first");
+const movieFormTwo = document.querySelector(".movie-form.second");
+
+console.log(blurOverlay);
+
+
+// Event listener for the + button
+const addButton = document.getElementById("button-container");
+addButton.addEventListener('click',  e => {
+    blurOverlay.style.display = "block";
+    movieFormOne.style.display = "block";
+    movieFormTwo.style.display = "none";
+    let inputForm = document.querySelectorAll(".movie-form input");
+    inputForm.forEach(input => {
+        input.value = "";
+    });
+});
+
+// Event listener to get to the second page
+const movieForm = document.querySelector(".manual-movie-form");
+movieForm.addEventListener('submit', e => {
+    e.preventDefault();
+    movieFormOne.style.display = "none";
+    movieFormTwo.style.display = "block";
+    const circle = document.querySelector(".movie-form.second .progress-circle.second");
+    circle.style.backgroundColor = "#20285b";
+});
+
+// Event listener to close the form
+const closeIcon = document.querySelectorAll(".movie-form .fa-regular.fa-circle-xmark");
+closeIcon.forEach(icon => {
+    icon.addEventListener('click', e => {
+    blurOverlay.style.display = "none";
+    movieFormOne.style.display = "none";
+    movieFormTwo.style.display = "none";
+    });
+});
